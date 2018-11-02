ee03465901c4cea043ce32d0cd5022be13aa0965
Esvin González <usac.ing.esvin@gmail.com>
2018-11-02 00:16:00 -0600

    Agregando carga de goleadores CSV.

---
+++ carga_goleadores_csv.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE master_goleador;
+CREATE TABLE public.master_goleador (
+    temporada character varying(50) NOT NULL,
+    jugador character varying(150) NOT NULL,
+    goles integer DEFAULT 0 NOT NULL
+);
+ALTER TABLE public.master_goleador OWNER TO postgres;
+COPY master_goleador FROM '/tmp/GoleadoresLaLiga.csv' WITH CSV HEADER;
