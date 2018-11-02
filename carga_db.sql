e8a44f3a0f9f3898cfb3b2dd19183530ce687c4f
Sarina20Lives <aniras.s20s@gmail.com>
2018-11-02 01:17:54 -0600

    Terminando el llenado de tablas desde tabla maestra de partidos

--- carga_db.sql
+++ carga_db.sql
@@ -1,4 +1,7 @@
-﻿DELETE FROM jornada;
+﻿DELETE FROM participacion;
+DELETE FROM partido;
+DELETE FROM equipo;
+DELETE FROM jornada;
 DELETE FROM temporada;
 
 INSERT INTO temporada (id_temporada, fecha_inicio, fecha_fin, nombre)
@@ -18,7 +21,22 @@ ALTER TABLE equipo ALTER COLUMN id_equipo SET DEFAULT nextval('seq_equipo');
 INSERT INTO equipo (nombre)
 SELECT DISTINCT TRIM(equipo_local) FROM master_partido;
 
+INSERT INTO partido (id_partido, id_local, id_visitante, fecha, goles_local, goles_visitante, id_jornada)
+SELECT nextval('seq_partido'), L.id_equipo AS local, V.id_equipo AS visitante, M.fecha, M.goles_local, M.goles_visitante, J.id_jornada 
+FROM master_partido AS M, temporada AS T, jornada AS J, equipo AS L, equipo AS V
+WHERE M.equipo_local = L.nombre
+AND M.equipo_visitante = V.nombre
+AND M.jornada = J.nombre
+AND M.temporada = T.nombre
+AND J.id_temporada = T.id_temporada;
 
-select * from equipo
-SELECT * FROM master_partido 
-WHERE equipo_local like '%Real Madrid	1%'
+ALTER TABLE participacion ALTER COLUMN id_participacion SET DEFAULT nextval('seq_participacion');
+
+INSERT INTO participacion (id_temporada, id_equipo)
+SELECT DISTINCT id_temporada, id_equipo
+FROM master_partido, temporada, equipo
+WHERE master_partido.temporada = temporada.nombre
+AND master_partido.equipo_local = equipo.nombre
+ORDER BY id_temporada;
+
+
